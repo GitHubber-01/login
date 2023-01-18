aaa877fc1e2f3b8627ecf7cd90a2e53cf16e5f0f
GitHubber-01 <73506900+GitHubber-01@users.noreply.github.com>
2023-01-18 06:01:20 +0100

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,5 +1,5 @@
 import { signOut, useSession } from "next-auth/react";
-import Link from "next/Link";
+import Link from "next/link";
 
 export default function Navbar() {
     const { data: session } = useSession();
